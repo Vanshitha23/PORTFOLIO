8668b432128736fcc792a48ddb2c62a034ac4fe4
vanshitha23 <vanshitha.prakash2327@gmail.com>
2025-05-07 21:02:19 +0530

    Set up GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/PORTFOLIO/', // 👈 use your repo name here
+});
